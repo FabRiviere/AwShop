976d82a1188c34f89e2ebc82622a88c25a388456
Fab Riviere <fabien@activateurweb.com>
2023-03-25 16:22:49 +0100

    Creation of the AwShop - e-commerce development project. Installation of Symfony V6.2 and configuration of the MySQL database. Creation of the Users entity with the maker and addition of fields according to MySQL schema. Addition of user authentication, and creation of Login and Register templates after modifying the controller. Creation, without using a bundle, of the verification of the activation of the user with the sending of an email of activation of the account (use of JWT Service). Creation of the JWTService (generating a token), and SendMailService (sending emails) services. Added email template. --------------- Création du projet de développement AwShop - e-commerce.Installation de Symfony V6.2 et paramètrage de la base de données MySQL. Création de l'entité Users avec le maker et ajout des champs suivant schéma MySQL. Ajout de l'authentification utilisateurs, et créations des templates Login et Register après modification du contrôleur. Création, sans utiliser de bundle, de la vérification de l'activation de l'utilisateur avec envoi d'un email d'activation du compte (utilisation de JWT Service). Créations des services JWTService(générer un token), et SendMailService(envoi des emails). Ajout template emails.

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use App\Form\RegistrationFormType;
+use App\Repository\UsersRepository;
+use App\Security\UsersAuthenticator;
+use App\Service\JWTService;
+use App\Service\SendMailService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher,
+                             UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator,
+                              EntityManagerInterface $entityManager, SendMailService $mail, JWTService $jwt): Response
+    {
+        $user = new Users();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            // On génère le JWT de l'utilisateur
+            // ; On crée le $header
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+            // ; On crée le payload
+            $payload = [
+                'user_id' => $user->getId()
+            ];
+            // ; On génère le token
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+            
+            // Envoie d'un mail
+            $mail->send(
+                'no-reply@awshop.fr',
+                $user->getEmail(),
+                'Activaton de votre compte sur le site e-commerce AwShop',
+                'register',
+                compact('user', 'token')
+            );
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    // Création d'une nouvelle route pour vérifier le token de l'utilisateur
+    #[Route('/verif/{token}', name: 'app_verify_user')]
+    public function verifyUser($token, JWTService $jwt, UsersRepository $usersRepository, EntityManagerInterface $manager): Response
+    {
+        // On vérifie si token est valide, n'a pas expiré et pas modifié
+        if ($jwt->isValid($token) && !$jwt->isExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret'))) {
+            
+            //; On récupère le payload
+            $payload = $jwt->getPayload($token);
+            
+            //; On récupère le user du token
+            $user = $usersRepository->find($payload['user_id']);
+
+            //; On vérifie que l'utilisateur existe et n'a pas encore activé son compte
+            if ($user && !$user->isIsVerified()) {
+                $user->setIsVerified(true);
+                $manager->flush($user);
+                $this->addFlash('token_message_success', 'Utilisateur activé ! 👍');
+                return $this->redirectToRoute('app_home');
+            } 
+        }
+        // Ici un problème se pose ds le token
+        $this->addFlash('token_message_error', 'Le token est invalide ou à expiré 😟');
+
+        return $this->redirectToRoute('app_login');
+    }
+
+    // Méthode pour renvoyer le lien d'activation si token expiré ou mail supprimé, etc.
+    #[Route('/revoiVerif', name: 'app_resend_verif')]
+    public function resendVerif(JWTService $jwt, SendMailService $mail, UsersRepository $usersRepository): Response
+    {
+        $user = $this->getUser();
+
+        if(!$user) {
+            $this->addFlash('resend_verif_error', 'Vous devez être connecté pour accéder à cette page');
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($user->isIsVerified()) {
+            $this->addFlash('resend_verif_warning', 'Cet utilisateur est déjà activé');
+            return $this->redirectToRoute('app_home');
+        }
+
+        // On reproduit l'envoi d'un token et d'un email
+        $header = [
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ];
+        $payload = [
+            'user_id' => $user->getId()
+        ];
+       
+        $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+        $mail->send(
+            'no-reply@awshop.fr',
+            $user->getEmail(),
+            'Activaton de votre compte sur le site e-commerce AwShop',
+            'register',
+            compact('user', 'token')
+        );
+        $this->addFlash('resend_verif_success', 'Email de vérification envoyé');
+            return $this->redirectToRoute('app_home');
+    }
+}
---
+++ src/Entity/Users.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\CreatedAtTrait;
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UsersRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class Users implements UserInterface, PasswordAuthenticatedUserInterface
+{
+   use CreatedAtTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column]
+    private ?bool $is_verified = false;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function isIsVerified(): ?bool
+    {
+        return $this->is_verified;
+    }
+
+    public function setIsVerified(bool $is_verified): self
+    {
+        $this->is_verified = $is_verified;
+
+        return $this;
+    }
+}
