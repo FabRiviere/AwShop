3dfb6e9886fc33946a6216c61bb394eb09a51366
Fab Riviere <fabien@activateurweb.com>
2023-03-30 19:37:43 +0200

    Affichage des produits sur page d'acceuil - Modification du HomeController - Affichage produit spécifique avec ajout des templates et des partials. Gestion de la relation entre produit suivant catégorie pour affichage dans la bas de la page produit en relatedProducts #15 #16

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Products;
+use App\Repository\ProductsRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductsRepository $productsRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $products = $productsRepository->findAll();
+
+        $productBestSeller = $productsRepository->findByIsBestSeller(1);
+
+        $productSpecialOffer = $productsRepository->findByIsSpecialOffer(1);
+
+        $productNewArrival = $productsRepository->findByIsNewArrival(1);
+
+        $productFeatured = $productsRepository->findByIsFeatured(1);
+        
+        return $this->render('home/index.html.twig', [
+            'products' => $products,
+            'productBestSeller' => $productBestSeller,
+            'productSpecialOffer' => $productSpecialOffer,
+            'productNewArrival' => $productNewArrival,
+            'productFeatured' => $productFeatured,
+        ]);
+    }
+
+    #[Route('/product/{slug}', name: 'app_product_details')]
+    public function show(?Products $product): Response
+    {
+        if (!$product) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/single_product.html.twig', [
+            'product' => $product,
+        ]);
     }
 }
